1099ea895fa691db7191b91aa2d9ad5bb7f506e5
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-03-15 19:45:13 +0100

    commit

--- src/main/java/dev/elma/clients/ClientGrpcCS.java
+++ src/main/java/dev/elma/clients/ClientGrpcCS.java
@@ -6,46 +6,50 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class ClientGrpcCS {
-    public static void main(String[] args) {
-        ManagedChannel connection = ManagedChannelBuilder.forAddress("localhost", 2001).usePlaintext().build();
-        //create Asynchronous Stub
-        bankServicesGrpc.bankServicesStub bankServicesStubAsyn = bankServicesGrpc.newStub(connection);
+    public static void main(String[] args) throws IOException {
+        ManagedChannel localhost =ManagedChannelBuilder.forAddress("localhost",2001).usePlaintext().build();
+        bankServicesGrpc.bankServicesStub bankServicesStub = bankServicesGrpc.newStub(localhost);
 
-        Timer timer=new Timer();
+        StreamObserver<BankServices.messageReq> messageReqStreamObserver = bankServicesStub.perfomCurrencyStream(new StreamObserver<BankServices.messageResp>() {
+            @Override
+            public void onNext(BankServices.messageResp messageResp) {
+                System.out.println("Response of server is: "+messageResp.getResult());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                //System.out.println("The last One...");
+            }
+        });
+
+        Timer timer = new Timer();
         timer.schedule(new TimerTask() {
+            int counter=0;
             @Override
             public void run() {
-
-                bankServicesStubAsyn.perfomCurrencyStream(new StreamObserver<BankServices.messageResp>() {
-                    int counter=0;
-                    @Override
-                    public void onNext(BankServices.messageResp messageResp) {
-                        BankServices.messageReq message = BankServices.messageReq.newBuilder().setMessageFrom("MAD").setMessageTo("$").setAmount(Math.random() * 1000).build();
-                        onNext(messageResp);
-                        if(counter==20){
-                            onCompleted();
-                            timer.cancel();
-                        }
-                        System.out.println(messageResp);
-
-                    }
-
-                    @Override
-                    public void onError(Throwable throwable) {
-
-                    }
-
-                    @Override
-                    public void onCompleted() {
-                        System.out.println("End");
-                    }
-                });
+                BankServices.messageReq messageReq = BankServices.messageReq.newBuilder().
+                        setMessageFrom("MAD").setMessageTo("EURO").setAmount(Math.random() * 100).build();
+                messageReqStreamObserver.onNext(messageReq);
+                if(counter++==20){
+                    messageReqStreamObserver.onCompleted();
+                    timer.cancel();
+                }
             }
-        },100,100);
+        }, 1000, 1000);
+
+        System.out.println("-----------Wait Resp----------");
+        System.in.read();
+
 
     }
 }
